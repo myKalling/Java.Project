02a19ec227e36ccba529ca5b613831678701c58b
myKalling <owner@mykalling.net>
2026-01-19 12:01:03 -0800

    ArrayUtils added

---
+++ ArrayUtils.java
@@ -0,0 +1,16 @@
+public class ArrayUtils {
+
+    /*
+    
+    Pasted information from project.
+    
+    */
+
+    Run | Debug
+    public static void main(String[] args) throws Exception{
+    System.out.println(sumOfEvents(new int[]{1, 2, 3, 4, 5, 6})); // Expected Output: 12
+    System.out.println(countOccurences(new int[]{2, 2, 2}, 2)); // Expected Output: 3
+
+    }
+}
+
