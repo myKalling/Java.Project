eed26fc330c3009a218dbbd5b4d57bc8b26eda87
myKalling <owner@mykalling.net>
2026-02-07 21:59:31 -0800

    ArraysArrayList created and commited to GitHub

---
+++ src/ArraysArrayList.java
@@ -0,0 +1,47 @@
+package src;
+
+import java.util.ArrayList;
+
+public class ArraysArrayList {
+    public static void main(String[] args) {
+        // Create an array of int and print all value and length
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("int[] length: " + numbers.length); // 5
+
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.println("index " + i + ": " + numbers[i]);
+            }
+
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(1);
+            list.add(2);
+            // list.add("string"); // This will cause a compile-time error because the list is defined to hold Integer objects
+            list.add(e: 3);
+            list.add(4);
+            list.add(5);
+            list.add(6); // This will work because ArrayList can grow dynamically
+
+            System.out.println("ArrayList size: " + list.size()); // 5
+
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println("index " + i + ": " + list.get(i));
+            }
+
+            // while loop
+            int index = 0;
+            while (index < list.size()) {
+                System.out.println("index " + index + ": " + list.get(index));
+                index++;
+            }
+
+            ArrayList<String> stringList = new ArrayList<>();
+            names.add(e: "Alice");
+            names.add(e: "Bob");
+            list.add(e: 123); // This will cause a compile-time error because the list is defined to hold String objects
+            names.add(e: "Charlie");
+
+            for (String names : stringList) {
+                System.out.println(names);
+            }
+    }
+}
