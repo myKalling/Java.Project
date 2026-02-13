3ee04a72891c109c77028086448cfb5f07c2d973
myKalling <owner@mykalling.net>
2026-02-13 15:30:55 -0800

    Changes to factorial method

--- src/RecursionPractice.java
+++ src/RecursionPractice.java
@@ -1,4 +1,24 @@
 public class RecursionPractice {
+    // Recursive method to calculate factorial of a number
+    public static int factorial(int n) {
+        
+        if (n==0) {
+            return 1; // base case: factorial of 0 is 1
+        } 
+        
+        return n * factorial(n - 1); // recursive case: n! = n * (n-1)!
+        // return (n == 0) ? 1 : n * factorial(n - 1);
+
+    }     
+        
+
+
+}
+
+
+
+
+
     public static void main(String[] args) {
         // Test the recursive method here
         /* 
@@ -19,7 +39,7 @@ public class RecursionPractice {
         // Test fibonacci
         int fibResult = fibonacci(7);
         System.out.println("7th Fibonacci number: " + fibResult);
-        
+
 
         int n = 5;
         System.out.println("Factorial of " + n + " is: " + factorial(n));
